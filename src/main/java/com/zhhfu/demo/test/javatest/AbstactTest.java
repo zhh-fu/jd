df81e6f5d9ee60051aaa94889ca64d28e73b1030
zhh_fu <zhh_fu@163.com>
2020-09-03 09:39:48 +0800

    测试代码

---
+++ src/main/java/com/zhhfu/demo/test/javatest/AbstactTest.java
@@ -0,0 +1,16 @@
+package com.zhhfu.demo.test.javatest;
+
+/**
+ * @author ：fuzhihang5
+ * @date ：Created in 2020/9/2 21:16
+ * @description ：抽象类测试
+ * @solution ：
+ */
+public abstract class AbstactTest {
+    private int v1;
+    public void print(){
+        System.out.println("this is a abstract test");
+    }
+
+    public void print1(){}
+}
